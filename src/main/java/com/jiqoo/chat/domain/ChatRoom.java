12ef4fc3edb4784f79d9fd53d29da3f17ba4a5d9
chaeseongsil <chaeseongsil@gmail.com>
2023-10-11 16:29:49 +0900

    [ADD] 23/10/11 16:18 채팅 기능 초기 커밋
    1. pom.xml gson 라이브러리 추가
    2. WebSocketConfig.java 추가
    3. 채팅 관련 도메인 추가
    4. chat-mapper.xml 추가

--- src/main/java/com/jiqoo/chat/domain/ChatRoom.java
+++ src/main/java/com/jiqoo/chat/domain/ChatRoom.java
@@ -1,5 +1,49 @@
 package com.jiqoo.chat.domain;
 
+import java.sql.Timestamp;
+
 public class ChatRoom {
+
+	private int chatNo;
+	private String chatName;
+	private int refMoqooNo;
+	private Timestamp cCreateDate;
+
+	public int getChatNo() {
+		return chatNo;
+	}
+
+	public void setChatNo(int chatNo) {
+		this.chatNo = chatNo;
+	}
+
+	public String getChatName() {
+		return chatName;
+	}
+
+	public void setChatName(String chatName) {
+		this.chatName = chatName;
+	}
+
+	public int getRefMoqooNo() {
+		return refMoqooNo;
+	}
+
+	public void setRefMoqooNo(int refMoqooNo) {
+		this.refMoqooNo = refMoqooNo;
+	}
+
+	public Timestamp getcCreateDate() {
+		return cCreateDate;
+	}
+
+	public void setcCreateDate(Timestamp cCreateDate) {
+		this.cCreateDate = cCreateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "채팅방 [채팅방번호=" + chatNo + ", 채팅방이름=" + chatName + ", 모임번호=" + refMoqooNo + ", 생성일자=" + cCreateDate + "]";
+	}
 
 }
--- src/main/java/com/jiqoo/chat/service/ChatService.java
+++ src/main/java/com/jiqoo/chat/service/ChatService.java
@@ -1,5 +1,28 @@
 package com.jiqoo.chat.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import com.jiqoo.chat.domain.ChatMessage;
+import com.jiqoo.chat.domain.ChatRoom;
+
 public interface ChatService {
+
+	/**
+	 * 아이디로 참여 채팅방 조회 Service
+	 * 
+	 * @param userId
+	 * @return List<ChatRoom>s
+	 */
+	List<ChatRoom> selectChatRoomById(String userId);
+
+	/**
+	 * 채팅방 별 메시지 조회 Service
+	 * 
+	 * @param chatNo
+	 * @return List<ChatMessage>
+	 */
+	List<ChatMessage> selectChatMessageByNo(int chatNo);
 
 }
