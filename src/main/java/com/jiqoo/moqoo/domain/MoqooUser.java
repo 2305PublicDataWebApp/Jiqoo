81af84007c44203f8b5d357397c576a3268bb104
jangjingyeong <jjg070593@gmail.com>
2023-10-11 16:47:53 +0900

    [ADD] 23/10/11 Moqoo관련 domain 생성
    1. Moqoo domain 생성
    2. MoqooUser domain 생성

--- src/main/java/com/jiqoo/moqoo/domain/MoqooUser.java
+++ src/main/java/com/jiqoo/moqoo/domain/MoqooUser.java
@@ -2,4 +2,26 @@ package com.jiqoo.moqoo.domain;
 
 public class MoqooUser {
 
+	private int refMoqooNo;
+	private String refUserId;
+	
+	
+	public int getRefMoqooNo() {
+		return refMoqooNo;
+	}
+	public void setRefMoqooNo(int refMoqooNo) {
+		this.refMoqooNo = refMoqooNo;
+	}
+	public String getRefUserId() {
+		return refUserId;
+	}
+	public void setRefUserId(String refUserId) {
+		this.refUserId = refUserId;
+	}
+	
+	@Override
+	public String toString() {
+		return "모임참여자 [모꾸번호=" + refMoqooNo + ", 참여자아이디=" + refUserId + "]";
+	}
+	
 }
