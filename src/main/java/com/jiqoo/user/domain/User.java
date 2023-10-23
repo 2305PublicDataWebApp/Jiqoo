5099767af4131c7b1e748016730a58382e345e7c
jgoooood <jgood7070@gmail.com>
2023-10-23 09:16:13 +0900

    [FIX] 23/10/23 (09:15) USER도메인 수정
    - USER도메인 수정 : 기윤님 도메인 대신 추가함

--- src/main/java/com/jiqoo/user/domain/User.java
+++ src/main/java/com/jiqoo/user/domain/User.java
@@ -1,6 +1,10 @@
 package com.jiqoo.user.domain;
 
 import java.sql.Timestamp;
+import java.util.List;
+
+import com.jiqoo.common.domain.Comment;
+import com.jiqoo.report.domain.Report;
 
 public class User {
 	
@@ -24,7 +28,9 @@ public class User {
 	
 	private int count;
 	private String ageGroup;
-	
+	private List<Report> reportList;
+	private List<Comment> comtList;
+	private int uReportCount;
 
 	private int followers; //나를 팔로우한 사람들
 	private int followings; //내가 팔로우한 사람들
@@ -222,6 +228,30 @@ public class User {
 
 	public void setMoqooCount(int moqooCount) {
 		this.moqooCount = moqooCount;
+	}
+
+	public List<Report> getReportList() {
+		return reportList;
+	}
+
+	public void setReportList(List<Report> reportList) {
+		this.reportList = reportList;
+	}
+
+	public List<Comment> getComtList() {
+		return comtList;
+	}
+
+	public void setComtList(List<Comment> comtList) {
+		this.comtList = comtList;
+	}
+
+	public int getuReportCount() {
+		return uReportCount;
+	}
+
+	public void setuReportCount(int uReportCount) {
+		this.uReportCount = uReportCount;
 	}
 
 	@Override
